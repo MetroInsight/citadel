b77fcecbb8b864ca4310618df4e0884deab7877a
Jason Koh <bk7749@gmail.com>
2017-03-07 19:17:22 -0800

    add config for test

---
+++ test/config.py
@@ -0,0 +1,33 @@
+# config parameters for flask
+# TODO: change to class structure for supporting multiple dev environments
+
+# Citadel Configuration
+CITADEL_HOST = '127.0.0.1'
+CITADEL_PORT = 8080
+SERVER_NAME = '127.0.0.1:8080'
+SECRET_KEY = "MetroInsight_Secret_Key_Make_This_Complicated"
+TOKEN_EXPIRATION = 3600
+#SWAGGER_VALIDATOR_URL = '0.0.0.0:5000'
+
+
+# Metadata DB Configuration 
+METADATA_DB_TYPE = 'mongodb'
+
+# Connection parameters for MongoDB
+MONGODB_DATABASE = 'citadel'
+MONGODB_HOST = '172.17.0.3'
+MONGODB_PORT = 27017
+
+# Timeseries DB Configuration
+TIMESERIES_DB_TYPE = 'influxdb'
+
+## Connection parameters for InfluxDB
+INFLUXDB_DATABASE = 'citadel'
+INFLUXDB_HOST = '172.17.0.2'
+INFLUXDB_PORT = 8086
+# TODO: change to less privileged user
+INFLUXDB_USERNAME = 'citadel'
+INFLUXDB_PASSWORD = 'citadel'
+
+# Use for development only
+DEBUG = True

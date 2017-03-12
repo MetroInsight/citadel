f6dcb28381eb8fa1bea4addbc04272902c7be335
Jason Koh <bk7749@gmail.com>
2017-03-11 20:43:31 -0800

    update

--- app/main.py
+++ app/main.py
@@ -13,25 +13,29 @@ def hello():
     return "Hello Citizens!"
 
 
-if __name__ == '__main__':
+def run():
     debug = True
     host = CITADEL_HOST
-    port = CITADEL_PORT
+#    port = CITADEL_PORT
     server_name = SERVER_NAME
 
     parser = argparse.ArgumentParser(description='Run Citadel web service.')
     parser.add_argument('-host', dest='host', default=host,
         help='ip bind address (default: ' + host + ')')
-    parser.add_argument('-p', dest='port', type=int, default=port,
-        help='port to listen on (default: ' + str(port) + ')')
+    #parser.add_argument('-p', dest='port', type=int, default=port,
+     #   help='port to listen on (default: ' + str(port) + ')')
 
     args = parser.parse_args()
 
     # set SERVER_NAME for swagger
     app.config['SERVER_NAME'] = server_name
 
-    with app.app_context():
-        with open('doc/api/swagger_schema.json', 'w') as fp:
-            json.dump(api.__schema__, fp)
+#    with app.app_context():
+#        with open('doc/api/swagger_schema.json', 'w') as fp:
+#            json.dump(api.__schema__, fp)
 
-    app.run(args.host, args.port, debug)
+    #app.run(host=args.host, port=args.port, debug=debug)
+    app.run(host=args.host, debug=debug)
+
+if __name__ == '__main__':
+    run()

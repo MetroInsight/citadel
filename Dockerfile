77e1c6c6eaba171b5eb8dd446a5eb10bd60fae29
Jason Koh <bk7749@gmail.com>
2017-06-19 22:21:40 -0700

    clean up for vertx repo

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM tiangolo/uwsgi-nginx-flask:flask-python3.5
-
-COPY requirements.txt .
-RUN pip install --upgrade pip
-RUN pip install -r requirements.txt
-
-ADD ./app /app
-
-
--- app/citadel/rest_api/responses.py
+++
@@ -1,6 +0,0 @@
-success_true = {'success': True}
-success_false = {'success': False}
-missing_data = {'success': False, 'error': 'Missing data'}
-missing_parameters = {'success': False, 'error': 'Missing parameters'}
-resolution_high = {'success': False, 'error': 'Too many points for this resolution'}
-duplicate_error = {'success': False, 'error': 'Entry already exists'}

e575b39c25d3955377bbb6500d2ed805e5ed7a0c
Jason Koh <bk7749@gmail.com>
2017-07-03 23:18:49 -0700

    add comments and some cleanup

--- src/main/java/metroinsight/citadel/MainVerticle.java
+++ src/main/java/metroinsight/citadel/MainVerticle.java
@@ -7,6 +7,7 @@ public class MainVerticle extends AbstractVerticle {
 	
 	@Override
 	public void start() throws Exception {
+	  // Deploy verticles.
     //vertx.deployVerticle(MetadataVerticle.class.getName());
     //vertx.deployVerticle(TimeseriesVerticle.class.getName());
     vertx.deployVerticle(RestApiVerticle.class.getName());

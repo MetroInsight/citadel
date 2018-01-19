6109289da27063127f3e3082d09afc059180efb0
Jason Koh <bk7749@gmail.com>
2018-01-18 17:41:18 -0800

    fix some error messages

--- src/main/java/metroinsight/citadel/common/ErrorMessages.java
+++ src/main/java/metroinsight/citadel/common/ErrorMessages.java
@@ -4,4 +4,6 @@ final public class ErrorMessages {
   public final static String EXISTING_POINT_NAME = "Existing name";
   public final static String EXISTING_UUID = "Existing uuid";
   public final static String SENSOR_NOT_FOUND = "Sensor not found";
+  public final static String EMPTY_SEC_TOKEN = "Security token is not given";
+  public final static String EMPTY_UUID = "UUID is not given";
 }

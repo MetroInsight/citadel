d8709425a89d78dc7529abe8a7f2c29e34b2f0e5
Jason Koh <bk7749@gmail.com>
2018-01-22 05:53:40 +0000

    update

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-java -jar target/Citadel-0.0.1-SNAPSHOT-fat.jar -conf ./src/main/resources/conf/citadel-conf.json 
+if [ -f "pid.txt" ]; then
+  echo "pid.txt already exists."
+else
+  nohup java -jar target/Citadel-0.0.1-SNAPSHOT-fat.jar -conf ./src/main/resources/conf/citadel-conf.json &
+  echo $! >> pid.txt
+fi
